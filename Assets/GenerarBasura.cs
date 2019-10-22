4d55f077828dc63bd1b2aa7d35847ae3d5d64f01
Conejosaurio <cuevasg@uninorte.edu.co>
2019-10-22 20:55:04 +0000

    spawn

---
+++ Assets/GenerarBasura.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerarBasura : MonoBehaviour
+{
+    public GameObject[] residuo;
+    public float spawnTime;
+    private Vector2 screenbounds;
+    int i; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenbounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(OleadaBasura());
+    }
+    private void SpawnBasura()
+    {
+        i = Random.Range(0, 5);
+        GameObject a = Instantiate(residuo[i]) as GameObject;
+        a.transform.position = new Vector2(Random.Range(-screenbounds.x, screenbounds.x), Random.Range(-screenbounds.y, screenbounds.y));
+    }
+    // Update is called once per frame
+    IEnumerator OleadaBasura()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            SpawnBasura();
+        }
+    }
+}
