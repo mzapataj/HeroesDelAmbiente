089a41f7218c2d5b9f76ee6bcff69ff86e07410e
Jorge Miguel Zapata Diaz <mzapataj@uninorte.edu.co>
2019-11-06 07:14:41 +0000

    el usuario puede agregar su nombre y corrigiendo varias cosas

---
+++ Assets/Scripts/ComandosBasicos.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComandosBasicos : MonoBehaviour
+{
+    public void CargarEscena(string NombreDeEscena)
+    {
+        
+        SceneManager.LoadSceneAsync(NombreDeEscena);
+
+    }
+
+    public void CargarEscenaGames()
+    {
+
+        HandlerSessionPlayer handlerSessionPlayer = new HandlerSessionPlayer();
+
+       
+
+        if (handlerSessionPlayer.NameUser.Equals(""))
+        {
+            handlerSessionPlayer.popUpMenu.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync("Games");
+        }
+
+    }
+}
---
+++ Assets/Scripts/HandlerSessionPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HandlerSessionPlayer
+{
+    public GameObject popUpMenu;
+
+    private string nameUser;
+    public string NameUser
+    {
+            get { return PlayerPrefs.GetString("username",""); }
+            set { PlayerPrefs.SetString("username", value); }
+    }
+
+    public HandlerSessionPlayer()
+    { 
+        popUpMenu = EmpadasNecesarias.FindObject(GameObject.Find("NewPlayerPopUp").gameObject,"Panel");
+
+        EmpadasNecesarias.FindObject(GameObject.Find("NewPlayerPopUp").gameObject, "Button")
+            .GetComponent<Button>().onClick.AddListener(NameInputButtonEvent);
+        
+    }
+
+    public void NameInputButtonEvent()
+    {
+        string name = EmpadasNecesarias.FindObject(GameObject.Find("NewPlayerPopUp")
+            .gameObject,"Panel").transform.Find("NameInput").GetComponent<InputField>().text;
+        
+
+        if (!string.IsNullOrEmpty(name)){
+            this.NameUser = name;
+            SceneManager.LoadScene("Games"); 
+        }
+    }
+    
+}
