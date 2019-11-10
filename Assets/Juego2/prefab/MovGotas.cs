a2069c5246c64a3612de3030de18b78a8fd4515f
Conejosaurio <cuevasg@uninorte.edu.co>
2019-11-10 13:02:50 +0000

    movimiento gotas

---
+++ Assets/Juego2/prefab/MovGotas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovGotas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1.0f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
