e2fdd5ec75676d2af4191b4645324124e679b3ad
Conejosaurio <cuevasg@uninorte.edu.co>
2019-11-10 16:54:40 +0000

    No Comments

--- Assets/Juego2/Lluvia.cs
+++ Assets/Juego2/Lluvia.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Lluvia : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject[] Gota;
+    public float respawnTime = 1.0f;
+    int i;
+    private Vector2 screenbounds;
+
     void Start()
     {
-        
+        screenbounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(goteo());
     }
-
-    // Update is called once per frame
-    void Update()
+    private void spawnGota()
     {
-        
+        i = Random.Range(0, 10);
+        GameObject a = Instantiate(Gota[i]) as GameObject;
+        a.transform.position = new Vector2(Random.Range(-screenbounds.x, screenbounds.x), screenbounds.y * -2);
     }
+    IEnumerator goteo()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnGota();
+        }
+    }
+    // Update is called once per frame
 }
--- Assets/Juego2/prefab/MovGotas.cs
+++ Assets/Juego2/prefab/MovGotas.cs
@@ -7,15 +7,22 @@ public class MovGotas : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 1.0f;
     private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, -speed);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < screenBounds.y)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
