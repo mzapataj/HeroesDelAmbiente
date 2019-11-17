b85508cd6e14a1a861e2ce761cb820a950a7503f
Conejosaurio <cuevasg@uninorte.edu.co>
2019-11-17 13:40:31 +0000

    Movimiento Mondy sin balde

---
+++ Assets/Juego2/prefab/Movimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mover(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")*0));
+    }
+    void mover(Vector2 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+}
